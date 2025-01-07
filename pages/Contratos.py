b392fedcb30b06df7ad10fa3cd05d7bad764ccd6
Nefex_Shadow <lelimasbrito@gmail.com>
2025-01-07 20:39:22 -0300

    Pagina Contratos Finalizada

--- pages/Contratos.py
+++ pages/Contratos.py
@@ -1,13 +1,19 @@
 import sqlite3
 import pandas as pd
 import streamlit as st
+import altair as alt
 
 conn = sqlite3.connect('./Movimentacao_Julho_2021.db')
 cursor = conn.cursor()
 
 st.title('Contratos')
 st.sidebar.markdown('Contratos')
+st.write('#### Esse trecho representa os valores que os contratos agregaram para cada município.')
 
+left_column, right_column = st.columns(2)
+
+left_column.write('#### Escolha os municípios de interesse e qual tipo de contrato gorstaria de analisar.')
+left_column.write('Note contratos diferentes tiveram seus valores somados, logo os valores apresentados na tabela são os valores totais do dia')
 
 query = '''
     SELECT DISTINCT
@@ -20,13 +26,11 @@ query = '''
 
 df = pd.read_sql_query(query, conn)
 
-municipios = st.multiselect(
-    "Escolha o Município", list(df['nome']), [df['nome'].values[0]]
+municipios = right_column.multiselect(
+    "###### Escolha o Município", list(df['nome']), [df['nome'].values[0]]
 )
 if not municipios:
     st.error("Por favor, selecione um Município.")
-
-st.write(municipios)
 
 query = '''
 SELECT DISTINCT
@@ -39,16 +43,24 @@ ORDER BY
 
 df = pd.read_sql_query(query, conn)
 
-variaveis = st.selectbox(
-    "Escolha a variável do Contrato", list(df['variavel'])
+variaveis = right_column.selectbox(
+    "###### Escolha a variável do Contrato", list(df['variavel'])
 )
+
+query = '''
+    SELECT DISTINCT data
+    FROM Contrato
+'''
+col =  pd.read_sql_query(query, conn)
+
+data = pd.DataFrame(columns=col['data'])
 
 for m in municipios:
     query = ('''
     SELECT DISTINCT
         M.mun_nome as Nome, C.data as Data, SUM(C.valor) as Valor
     FROM
-        Contrato as C JOIN (SELECT DISTINCT M.nome as mun_nome, G.codigo as gas_codigo
+        Contrato as C RIGHT JOIN (SELECT DISTINCT M.nome as mun_nome, G.codigo as gas_codigo
             FROM Instalacao_de_Gasoduto as G JOIN Municipio as M
             ON G.codigo_municipio = M.codigo
             WHERE M.nome = "replace_mun") as M
@@ -62,7 +74,41 @@ for m in municipios:
         M.mun_nome
     ''').replace('replace_mun', m).replace('replace_var', variaveis)
     df = pd.read_sql_query(query, conn)
-    print(df)
-    df.pivot(index='Nome', columns='Data', values='Valor')
-    print(df)
-    st.write(df)
+    if (df.empty):
+        data.loc[m] = 0;
+    else:
+        data = data._append(df.pivot(index='Nome', columns='Data', values='Valor'))
+
+st.write('')
+st.write('### Valores')
+st.write(data)
+st.write('')
+
+if data.empty:
+    st.write("###### Nada a ser desenhado");
+
+else: 
+    data = data.T.reset_index()
+    if {'index'}.issubset(data.columns):
+        data = pd.melt(data, id_vars=["index"]).rename(columns={"variable":"Município", "index": "Dia", "value": "Valor"})
+    else:
+        data = pd.melt(data, id_vars=["data"]).rename(columns={"variable":"Município", "data": "Dia", "value": "Valor"})
+    st.write()
+    for i in range(0, 31 * len(municipios)):
+        data['Dia'].values[i] = data['Dia'].values[i].replace('07', data['Dia'].values[i][0] + data['Dia'].values[i][1])
+        data['Dia'].values[i] = data['Dia'].values[i].replace(data['Dia'].values[i][0] + data['Dia'].values[i][1], '07', 1)
+
+    st.write(data)
+    chart = (
+        alt.Chart(data)
+        .mark_area(opacity=0.3)
+        .encode(
+            x="Dia:T",
+            y=alt.Y("Valor:Q", stack=None),
+            color="Município:N",
+        )
+    )
+    st.write('')
+    st.write('### Gráfico')
+
+    st.altair_chart(chart, use_container_width=True)
